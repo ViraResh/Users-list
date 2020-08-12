e60db2210cbf854be7472fe27edecb6fd3319719
ViraResh <viraresh@ukr.net>
2020-08-12 11:52:25 +0300

    lesson 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatListModule } from '@angular/material/list';
-import {MatIconModule} from "@angular/material/icon";
+import { UsersListComponent } from './users-list/users-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatListModule,
-    MatIconModule
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
